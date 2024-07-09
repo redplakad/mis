286de6d7f30e24f95ee8be469a3f310bf16a68e3
redplakad <redplakad@gmail.com>
2024-07-10 06:52:10 +0700

    update

--- resources/views/Dashboard/index.blade.php
+++ resources/views/Dashboard/index.blade.php
@@ -1,35 +1,34 @@
 @extends('layouts.default')
 @section('contents')
+	<!--begin::Wrapper container-->
+	<div class="app-container container-xxl">
+		<!--begin::Main-->
+		<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
+			<!--begin::Content wrapper-->
+			<div class="d-flex flex-column flex-column-fluid">
+				<!--begin::Content-->
+				<div id="kt_app_content" class="app-content flex-column-fluid">
+					<!--begin::Row-->
+					@include('Dashboard.index.card')
+					<!--end::Row-->
+					<!--begin::Row-->
+					@include('Dashboard.index.statistik')
+					<!--end::Row-->
+					<!--begin::Row-->
+					@include('Dashboard.index.segmentasi')
+					<!--end::Row-->
 
-					<!--begin::Wrapper container-->
-					<div class="app-container container-xxl">
-						<!--begin::Main-->
-						<div class="app-main flex-column flex-row-fluid" id="kt_app_main">
-							<!--begin::Content wrapper-->
-							<div class="d-flex flex-column flex-column-fluid">
-								<!--begin::Content-->
-								<div id="kt_app_content" class="app-content flex-column-fluid">
-									<!--begin::Row-->
-									@include('Dashboard.index.card')
-									<!--end::Row-->
-									<!--begin::Row-->
-									@include('Dashboard.index.statistik')
-									<!--end::Row-->
-									<!--begin::Row-->
-                                    @include('Dashboard.index.segmentasi')
-									<!--end::Row-->
-
-								</div>
-								<!--end::Content-->
-							</div>
-							<!--end::Content wrapper-->
-							<!--begin::Footer-->
-							@include('layouts.footer')
-							<!--end::Footer-->
-						</div>
-						<!--end:::Main-->
-					</div>
-					<!--end::Wrapper container-->
+				</div>
+				<!--end::Content-->
+			</div>
+			<!--end::Content wrapper-->
+			<!--begin::Footer-->
+			@include('layouts.footer')
+			<!--end::Footer-->
+		</div>
+		<!--end:::Main-->
+	</div>
+	<!--end::Wrapper container-->
 @endsection
 @push('javascript')
 <script>
