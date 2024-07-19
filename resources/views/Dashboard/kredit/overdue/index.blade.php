d3468b37989f22307b1565e57269817c815e9e82
Red Plakad <redplakad@gmail.com>
2024-07-19 16:37:58 +0700

    fix minor bugs

--- resources/views/Dashboard/kredit/overdue/index.blade.php
+++ resources/views/Dashboard/kredit/overdue/index.blade.php
@@ -25,8 +25,9 @@
                                             <div class="d-flex align-items-center position-relative my-1 mb-2 mb-md-0">
                                                 <i class="ki-duotone ki-magnifier fs-1 position-absolute ms-6"><span class="path1"></span><span
                                                         class="path2"></span></i> <input type="text" data-kt-docs-table-filter="search"
-                                                    class="form-control form-control-solid w-250px ps-15" placeholder="Search Customers">
+                                                    class="form-control form-control-solid w-250px ps-15" placeholder="Search Customers" id="keyword">
                                             </div>
+
                                         </form>
                                         <!--end::Search-->
 
